d14743450c0f04878c649a0dca83fdac64985ea5
Daniel (dnl) Chaves <dnlgamelab@gmail.com>
2022-07-22 15:50:43 -0300

    Create APIs Viewset

--- department/views.py
+++ department/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Department
+from .serializers import DepartmentSerializer
 
 # Create your views here.
+
+class DeparmentViewSet(viewsets.ModelViewSet):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
