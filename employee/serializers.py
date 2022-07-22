aa2378b5ca5a9e74d644e2494228c1d25df9e77b
Daniel (dnl) Chaves <dnlgamelab@gmail.com>
2022-07-22 15:50:29 -0300

    Fix Employee Serialiozer ClassName

--- employee/serializers.py
+++ employee/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Employee
 
-class DepartmentSerializer(serializers.HyperlinkedModelSerializer):
+class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Employee
         fields =['name' 'email', 'department']
