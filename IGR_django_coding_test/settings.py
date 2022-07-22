d0a98a295bd9d5c996fab51c59110caa14853b3a
Daniel (dnl) Chaves <dnlgamelab@gmail.com>
2022-07-22 15:23:22 -0300

    Add dpartment and employee models and admins

--- IGR_django_coding_test/settings.py
+++ IGR_django_coding_test/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django',
+    'employee',
+    'department',
     'rest_framework'
 ]
 
--- employee/models.py
+++ employee/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from department.models import Department
 
 # Create your models here.
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=250)
+    department = models.ForeignKey(to= Department, on_delete=models.SET_NULL, null=True, blank=True)
